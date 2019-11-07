33b57dc617564c42217417b2cfcc741f31f93292
Uijeong97 <uiui_97@naver.com>
2019-11-07 19:05:40 +0000

    modify algo file

--- Pose_estimate/algo/posture_dist.py
+++ Pose_estimate/algo/posture_dist.py
@@ -1,15 +1,17 @@
 def check_waist(people_pose):
-    Neck = people_pose[1]
-    RHeap,LHeap = people_pose[8],people_pose[11]
+    Neck = people_pose[2:4]
+    RHeap,LHeap = people_pose[4:6],people_pose[10:12]
+
     MHeap = ((RHeap[0]+LHeap[0])/2, (RHeap[1]+LHeap[1])/2)
     
     if MHeap[0]!=Neck[0]:
+        return True
+    else:
         return False
-    else return True
         
 def check_knee(people_pose):
-    RKnee,RAnkle=people_pose[9],people_pose[10]
-    LKnee,LAnkle=people_pose[12],people_pose[13]
+    RKnee,RAnkle = people_pose[6:8],people_pose[8:10]
+    LKnee,LAnkle = people_pose[12:14],people_pose[14:]
     
     # compare x point
     ChKnee = RKnee if RKnee[0]<LKnee[0] else LKnee
